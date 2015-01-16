4df9718a6f50798f570678ae09843c3fb7a79bf3
rohils <rohils74@gmail.com>
2015-01-15 19:08:31 -0500

    Readded Create User
    Create user was added back to manage users.

--- manage_users.php
+++ manage_users.php
@@ -60,6 +60,37 @@
             </table>
         </div>
     </div>
+    <div class ="page-header">
+        <h3>Register</h3>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title">Create User</h2>
+        </div>
+        <div class="panel-body">
+            <form action="create_user.php" method="post">
+                <div class="input-group">
+                    <span class="input-group-addon" id="email-addon">@</span>
+                    <input type="text" name="email" class="form-control" placeholder="Email" aria-describedby="email-addon">
+                </div>
+                <div class="input-group">
+                    <span class="input-group-addon" id="password-addon">*</span>
+                    <input type="password" name="password" class="form-control" placeholder="Password" aria-describedby="password-addon">
+                </div>
+                <div class="radio">
+                    <label><input type="radio" name="rank" value="1">Admin</label>
+                </div>
+                <div class="radio">
+                    <label><input type="radio" name="rank" value="2">Data Analyst</label>
+                </div>
+                <div class="radio">
+                    <label><input type="radio" name="rank" value="3">Scouter</label>
+                </div>
+                <br>
+                <input type="submit" value="Create" class="btn">
+            </form>
+        </div>
+    </div>
 </div>
 <?php
     include('footer.php');
