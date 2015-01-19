905737a3f3d3df2c69abf27d87f2ad9e676df004
Jason Wu <jasonwupilly1997@gmail.com>
2015-01-19 14:37:34 -0500

    delete_event finished

--- delete_event.php
+++ delete_event.php
@@ -1,8 +1,44 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start();
+include('config.php');
+?>
+<?php
 
+if (!isset($_SESSION['user']) || $_SESSION['rank'] > 1) {
+    header('Location: index.php');
+}
+if (isset($_GET['id'])) {
+    $delete_id = $_GET['id'];
+    $find_query = mysql_query("SELECT * FROM events WHERE id=" . $delete_id);
+    if (mysql_num_rows($find_query) == 1) {
+        $record = mysql_fetch_array($find_query);
+        $prefix_name = $record['prefixName'];
+        $matches_name = $prefix_name . "_matches";
+        $data_name = $prefix_name . "_data";
+        $teams_name = $prefix_name . "_teams";
+        $prescout_name = $prefix_name . "_prescout";
+        $db_query = "DROP TABLE " . $matches_name;
+        if (!mysql_query($db_query)) {
+            die(mysql_error());
+        }
+        $db_query = "DROP TABLE " . $data_name;
+        if (!mysql_query($db_query)) {
+            die(mysql_error());
+        }
+        $db_query = "DROP TABLE " . $teams_name;
+        if (!mysql_query($db_query)) {
+            die(mysql_error());
+        }
+        $db_query = "DROP TABLE " . $prescout_name;
+        if (!mysql_query($db_query)) {
+            die(mysql_error());
+        }
+        $db_query = "DELETE FROM events WHERE id=" . $delete_id;
+        if (!mysql_query($db_query)) {
+            die(mysql_error());
+        }
+    }
+}
+header('Location: manage_events.php');
+?>
