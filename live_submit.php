98fdf1c30a819e5596b4100228ad56f05d705016
Jason Wu <jasonwupilly1997@gmail.com>
2015-02-26 12:00:37 -0500

    ugly find replace code

--- live_submit.php
+++ live_submit.php
@@ -13,66 +13,66 @@ if (mysql_num_rows($field_query) != 1) {
     header('Location: live_scout.php');
 }
 $record = mysql_fetch_array($field_query);
-$enteredBy = $_POST['enteredBy'];
-$matchNumber = $_POST['matchNumber'];
-$teamNumber = $_POST['teamNumber'];
-$botNumber = $_POST['botNumber'];
-$isDead = $_POST['isDead'];
+$enteredBy = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['enteredBy']);
+$matchNumber = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['matchNumber']);
+$teamNumber = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['teamNumber']);
+$botNumber = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['botNumber']);
+$isDead = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['isDead']);
 if (!isset($isDead) || $isDead != '1') {
     $isDead = '0';
 }
-$isShow = $_POST['isShow'];
+$isShow = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['isShow']);
 if (!isset($isShow) || $isShow != '1') {
     $isShow = '0';
 }
-$start = $_POST['start'];
-$yellowScored = $_POST['yellowScored'];
-$yellowPossess = $_POST['yellowPossess'];
-$yellowStack0 = $_POST['yellowStack0'];
+$start = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['start']);
+$yellowScored = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['yellowScored']);
+$yellowPossess = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['yellowPossess']);
+$yellowStack0 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['yellowStack0']);
 if (!isset($yellowStack0) || $yellowStack0 != '1') {
     $yellowStack0 = '0';
 }
-$yellowStack1 = $_POST['yellowStack1'];
+$yellowStack1 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['yellowStack1']);
 if (!isset($yellowStack1) || $yellowStack1 != '1') {
     $yellowStack1 = '0';
 }
-$yellowStack2 = $_POST['yellowStack2'];
+$yellowStack2 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['yellowStack2']);
 if (!isset($yellowStack2) || $yellowStack2 != '1') {
     $yellowStack2 = '0';
 }
-$autoContainerMoved = $_POST['autoContainerMoved'];
-$stepContainerMoved = $_POST['stepContainerMoved'];
-$mobility = $_POST['mobility'];
+$autoContainerMoved = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['autoContainerMoved']);
+$stepContainerMoved = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['stepContainerMoved']);
+$mobility = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['mobility']);
 if (!isset($mobility) || $mobility != '1') {
     $mobility = '0';
 }
-$greyToteMoved = $_POST['greyToteMoved'];
-$autoComments = $_POST['autoComments'];
-$totePossess = $_POST['totePossess'];
-$tote0 = $_POST['tote0'];
-$tote1 = $_POST['tote1'];
-$tote2 = $_POST['tote2'];
-$tote3 = $_POST['tote3'];
-$tote4 = $_POST['tote4'];
-$tote5 = $_POST['tote5'];
-$containerPossess = $_POST['containerPossess'];
-$container0 = $_POST['container0'];
-$container1 = $_POST['container1'];
-$container2 = $_POST['container2'];
-$container3 = $_POST['container3'];
-$container4 = $_POST['container4'];
-$container5 = $_POST['container5'];
-$noodleTrash = $_POST['noodleTrash'];
-$noodleLand = $_POST['noodleLand'];
-$noodleOther = $_POST['noodleOther'];
-$coopTote0 = $_POST['coopTote0'];
-$coopTote1 = $_POST['coopTote1'];
-$coopTote2 = $_POST['coopTote2'];
-$coopTote3 = $_POST['coopTote3'];
-$pickupLand = $_POST['pickupLand'];
-$pickupOther = $_POST['pickupOther'];
-$pickupHuman = $_POST['pickupHuman'];
-$teleComments = $_POST['teleComments'];
+$greyToteMoved = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['greyToteMoved']);
+$autoComments = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['autoComments']);
+$totePossess = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['totePossess']);
+$tote0 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['tote0']);
+$tote1 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['tote1']);
+$tote2 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['tote2']);
+$tote3 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['tote3']);
+$tote4 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['tote4']);
+$tote5 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['tote5']);
+$containerPossess = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['containerPossess']);
+$container0 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['container0']);
+$container1 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['container1']);
+$container2 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['container2']);
+$container3 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['container3']);
+$container4 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['container4']);
+$container5 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['container5']);
+$noodleTrash = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['noodleTrash']);
+$noodleLand = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['noodleLand']);
+$noodleOther = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['noodleOther']);
+$coopTote0 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['coopTote0']);
+$coopTote1 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['coopTote1']);
+$coopTote2 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['coopTote2']);
+$coopTote3 = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['coopTote3']);
+$pickupLand = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['pickupLand']);
+$pickupOther = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['pickupOther']);
+$pickupHuman = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['pickupHuman']);
+$teleComments = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['teleComments']);
 $data_name = $record['prefixName'] . "_data";
 $db_query = "INSERT INTO " . $data_name . "(enteredBy, matchNumber, teamNumber, botNumber, isDead, isShow, start, yellowScored, yellowPossess, yellowStack0, yellowStack1, yellowStack2, autoContainerMoved, stepContainerMoved, mobility, greyToteMoved, autoComments, totePossess, tote0, tote1, tote2, tote3, tote4, tote5, containerPossess, container0, container1, container2, container3, container4, container5, noodleTrash, noodleLand, noodleOther, coopTote0, coopTote1, coopTote2, coopTote3, pickupLand, pickupOther, pickupHuman, teleComments) VALUES('" . $enteredBy . "','" . $matchNumber . "','" . $teamNumber . "','" . $botNumber . "','" . $isDead . "','" . $isShow . "','" . $start . "','" . $yellowScored . "','" . $yellowPossess . "','" . $yellowStack0 . "','" . $yellowStack1 . "','" . $yellowStack2 . "','" . $autoContainerMoved . "','" . $stepContainerMoved . "','" . $mobility . "','" . $greyToteMoved . "','" . $autoComments . "','" . $totePossess . "','" . $tote0 . "','" . $tote1 . "','" . $tote2 . "','" . $tote3 . "','" . $tote4 . "','" . $tote5 . "','" . $containerPossess . "','" . $container0 . "','" . $container1 . "','" . $container2 . "','" . $container3 . "','" . $container4 . "','" . $container5 . "','" . $noodleTrash . "','" . $noodleLand . "','" . $noodleOther . "','" . $coopTote0 . "','" . $coopTote1 . "','" . $coopTote2 . "','" . $coopTote3 . "','" . $pickupLand . "','" . $pickupOther . "','" . $pickupHuman . "','" . $teleComments . "')";
 if (!mysql_query($db_query)) {
--- pre_submit.php
+++ pre_submit.php
@@ -13,38 +13,38 @@ if (mysql_num_rows($field_query) != 1) {
     header('Location: live_scout.php');
 }
 $record = mysql_fetch_array($field_query);
-$teamNumber = $_POST['teamNumber'];
-$talkedTo = $_POST['talkedTo'];
-$goals = $_POST['goals'];
-$manipulator = $_POST['manipulator'];
-$wheels = $_POST['wheels'];
-$auto = $_POST['auto'];
+$teamNumber = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['teamNumber']);
+$talkedTo = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['talkedTo']);
+$goals = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['goals']);
+$manipulator = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['manipulator']);
+$wheels = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['wheels']);
+$auto = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['auto']);
 if (!isset($auto) || $auto != '1') {
     $auto = '0';
 }
-$autoStrategy = $_POST['autoStrategy'];
-$autoMobility = $_POST['autoMobility'];
+$autoStrategy = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['autoStrategy']);
+$autoMobility = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['autoMobility']);
 if (!isset($autoMobility) || $autoMobility != '1') {
     $autoMobility = '0';
 }
-$autoPushTotes = $_POST['autoPushTotes'];
-$autoStackTotes = $_POST['autoStackTotes'];
-$autoPushContainers = $_POST['autoPushContainers'];
-$coop = $_POST['coop'];
+$autoPushTotes = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['autoPushTotes']);
+$autoStackTotes = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['autoStackTotes']);
+$autoPushContainers = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['autoPushContainers']);
+$coop = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['coop']);
 if (!isset($coop) || $coop != '1') {
     $coop = '0';
 }
-$teleStrategy = $_POST['teleStrategy'];
-$teleStackTotes = $_POST['teleStackTotes'];
-$teleScoringPlatform = $_POST['teleScoringPlatform'];
+$teleStrategy = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['teleStrategy']);
+$teleStackTotes = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['teleStackTotes']);
+$teleScoringPlatform = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['teleScoringPlatform']);
 if (!isset($teleScoringPlatform) || $teleScoringPlatform != '1') {
     $teleScoringPlatform = '0';
 }
-$telePushContainer = $_POST['telePushContainer'];
+$telePushContainer = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['telePushContainer']);
 if (!isset($telePushContainer) || $telePushContainer != '1') {
     $telePushContainer = '0';
 }
-$teleNoodleContainer = $_POST['teleNoodleContainer'];
+$teleNoodleContainer = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['teleNoodleContainer']);
 if (!isset($teleNoodleContainer) || $teleNoodleContainer != '1') {
     $teleNoodleContainer = '0';
 }
@@ -55,7 +55,7 @@ if ($_FILES['media']['name']) {
     $upload_file = $upload_dir . $time . basename($_FILES['media']['name']);
     move_uploaded_file($_FILES['media']['tmp_name'], $upload_file);
 }
-$additionalComments = $_POST['additionalComments'];
+$additionalComments = preg_replace("/[^A-Za-z0-9-\?\!\$\#\@\(\)\"\'\.\:\;\\@\,\_ \=\/\<\> ]/",'',$_POST['additionalComments']);
 $prescout_name = $record['prefixName'] . "_prescout";
 $pictures = $upload_file; 
 $db_query = "INSERT INTO " . $prescout_name . "(teamNumber, talkedTo, goals, manipulator, wheels, auto, autoStrategy, autoMobility, autoPushTotes, autoStackTotes, autoPushContainers, coop, teleStrategy, teleStackTotes, teleScoringPlatform, telePushContainer, teleNoodleContainer, pictures, additionalComments) VALUES('" . $teamNumber . "','" . $talkedTo . "','" . $goals . "','" . $manipulator . "','" . $wheels . "','" . $auto . "','" . $autoStrategy . "','" . $autoMobility . "','" . $autoPushTotes . "','" . $autoStackTotes . "','" . $autoPushContainers . "','" . $coop . "','" . $teleStrategy . "','" . $teleStackTotes . "','" . $teleScoringPlatform . "','" . $telePushContainer . "','" . $teleNoodleContainer . "','" . $pictures . "','" . $additionalComments . "')";
