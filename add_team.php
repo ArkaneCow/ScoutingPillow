244849871c654836bd1a3e3279d3495c0b01971e
Jason Wu <jasonwupilly1997@gmail.com>
2015-02-28 11:54:32 -0500

    fixed missing dollar sign

--- add_team.php
+++ add_team.php
@@ -21,7 +21,7 @@ if (mysql_num_rows($field_query) != 1) {
 $record = mysql_fetch_array($field_query);
 $teams_name = $record['prefixName'] . "_teams";
 $teams_query = mysql_query("INSERT INTO " . $teams_name . "(teamNumber, teamName) VALUES(" . $_POST['teamNumber'] . "," . $_POST['teamName'] . ")");
-if (!teams_query) {
+if (!$teams_query) {
     die(mysql_error());
 }
 header('Location: team_list.php?id=' . $_GET['id']);
