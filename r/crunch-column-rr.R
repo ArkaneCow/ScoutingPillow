67cbc836ded1d125d3f70a490e293e0dcc46bf73
Jason Wu <jasonwupilly1997@gmail.com>
2015-03-03 14:43:34 -0500

    tweaking r script

--- r/crunch-column-rr.R
+++ r/crunch-column-rr.R
@@ -1,18 +1,26 @@
 args <- commandArgs(trailingOnly = TRUE)
-data_input <- args[1]
-img_output <- args[2]
-png(img_output)
+#data_input <- args[1]
+#img_output <- args[2]
+data_input <- "test.csv"
+img_output <- "out.png"
+#png(img_output)
 data <- read.csv(data_input, header=TRUE)
 data_fields = data[,1]
 data_values = data[,2]
 
-res_lm <- lm(as.formula(paste(colnames(data)[2], "~ c(1:length(data_values))")), data=data)
+data_index <- c(1:length(data_values))
+res_lm <- lm(as.formula(paste(colnames(data)[2], "~ data_index")), data=data)
 res_coeffs <- coefficients(res_lm)
-next_x = length(data_values) + 1
-res <- res_coeffs[1] + res_coeffs[2]*next_x
+next_x = data.frame(data_index = length(data_values) + 1)
+next_x_val = length(data_values) + 1
+res_predict <- predict(res_lm, next_x, interval="predict")
+res <- res_predict[1]
+res_lwr <- res_predict[2]
+res_upr <- res_predict[3]
 
+data_index <- append(data_index, next_x_val)
 data_plot <- append(data_values, res)
-plot(data_plot, type="n", axes=FALSE, ann=FALSE, col="black")
+plot(data_index, data_plot, type="n", axes=FALSE, ann=FALSE, col="black")
 text(c(1:length(data_plot)), data_plot, round(data_plot, 2), cex=0.9)
 lines(data_values, type="l", pch=22, col="black")
 axis(1, at=1:length(data_values), lab=c(paste(data_fields)))
@@ -23,10 +31,10 @@ abline(res_lm, col="blue")
 data_extend = c(length(data_values), length(data_values) + 1)
 data_predict = c(data_values[length(data_values)], res)
 lines(data_extend, data_predict, type="l", lty=2, lwd=2, pch=22, col="red")
-data_summary = summary(data_values)
+plot(c(next_x_val, next_x_val), c(res_lwr, res_upr), type="n")
 text(length(data_values), mean(data_values), mean(data_summary), cex=0.8)
-abline(h=mean(data_summary), col="green")
+abline(h=mean(data_values), col="green")
 box()
 
-dev.off()
+#dev.off()
 
