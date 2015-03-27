d23f704e8f894462f42a0d5f61434dce6e626ce1
Jason Wu <jasonwupilly1997@gmail.com>
2015-03-27 17:02:08 -0400

    more stats

--- rank_list.php
+++ rank_list.php
@@ -44,13 +44,17 @@ include('header.php');
                         <th>Avg Pts</th>
                         <th>Min Pts</th>
                         <th>Max Pts</th>
+                        <th>Tote Pts</th>
+                        <th>Container Pts</th>
+                        <th>Human Pts</th>
+                        <th>Landfill Pts</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
                     $rank_count = 1;
-                    $db_query = mysql_query("SELECT teamNumber, AVG((2*(tote0 + tote1 + tote2 + tote3 + tote4 + tote5) + 6*noodleTrash + container0*1*4 + container1*2*4 + container2*3*4 + container3*4*4 + container4*5*4 + container5*6*4)) AS avgScore, MIN((2*(tote0 + tote1 + tote2 + tote3 + tote4 + tote5) + 6*noodleTrash + container0*1*4 + container1*2*4 + container2*3*4 + container3*4*4 + container4*5*4 + container5*6*4)) minScore, MAX((2*(tote0 + tote1 + tote2 + tote3 + tote4 + tote5) + 6*noodleTrash + container0*1*4 + container1*2*4 + container2*3*4 + container3*4*4 + container4*5*4 + container5*6*4)) maxScore FROM " . $data_name . " GROUP BY teamNumber ORDER BY avgScore DESC");
+                    $db_query = mysql_query("SELECT teamNumber, AVG((2*(tote0 + tote1 + tote2 + tote3 + tote4 + tote5) + 6*noodleTrash + container0*1*4 + container1*2*4 + container2*3*4 + container3*4*4 + container4*5*4 + container5*6*4)) AS avgScore, MIN((2*(tote0 + tote1 + tote2 + tote3 + tote4 + tote5) + 6*noodleTrash + container0*1*4 + container1*2*4 + container2*3*4 + container3*4*4 + container4*5*4 + container5*6*4)) minScore, MAX((2*(tote0 + tote1 + tote2 + tote3 + tote4 + tote5) + 6*noodleTrash + container0*1*4 + container1*2*4 + container2*3*4 + container3*4*4 + container4*5*4 + container5*6*4)) maxScore, AVG(2 * (tote0 + tote1 + tote2 + tote3 + tote4 + tote5)) toteScore, AVG(container0*1*4 + container1*2*4 + container2*3*4 + container3*4*4 + container4*5*4 + container5*6*4) containerScore, AVG(pickupHuman * 2) humanScore, AVG(pickupLand * 2) landfillScore FROM " . $data_name . " GROUP BY teamNumber ORDER BY avgScore DESC");
                     while ($team = mysql_fetch_array($db_query)) {
                         echo("<tr>");
                         echo("<td scope=\"row\">");
@@ -67,6 +71,18 @@ include('header.php');
                         echo("</td>");
                         echo("<td scope=\"row\">");
                         echo($team['maxScore']);
+                        echo("</td>");
+                        echo("<td scope=\"row\">");
+                        echo($team['toteScore']);
+                        echo("</td>");
+                        echo("<td scope=\"row\">");
+                        echo($team['containerScore']);
+                        echo("</td>");
+                        echo("<td scope=\"row\">");
+                        echo($team['humanScore']);
+                        echo("</td>");
+                        echo("<td scope=\"row\">");
+                        echo($team['landfillScore']);
                         echo("</td>");
                         echo("<td scope=\"row\">");
                         echo("<a href=\"view_team.php?id=" . $_GET['id'] . "&team=" . $team['teamNumber'] . "\" class=\"btn\"><span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span>View</a>");
