e9f855ff731fac6e517b718f9ed3f226729a5780
Jason Wu <jasonwupilly1997@gmail.com>
2015-03-27 14:47:03 -0400

    ranking list page

---
+++ rank_list.php
@@ -0,0 +1,70 @@
+<?php
+session_start();
+include('config.php');
+?>
+<?php
+if (!isset($_SESSION['user'])) {
+    header('Location: index.php');
+}
+if (!isset($_GET['id'])) {
+    header('Location: view_events.php');
+}
+$field_query = mysql_query("SELECT * FROM events WHERE id=" . $_GET['id']);
+if (mysql_num_rows($field_query) != 1) {
+    header('Location: view_events.php');
+}
+$record = mysql_fetch_array($field_query);
+$teams_name = $record['prefixName'] . "_teams";
+$prescout_name = $record['prefixName'] . "_prescout"
+?>
+<?php
+include('header.php');
+?>
+<div class="container">
+    <div class="page-header">
+        <h3>Team Rankings</h3>
+        <br>
+        <h4>
+            <?php
+            echo($record['eventName']);
+            ?>
+        </h4>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h2 class="panel-title">Team Contribution Rankings</h2>
+        </div>
+        <div class="panel-body">
+            <table class="container">
+                <thead>
+                    <tr>
+                        <th>Team #</th>
+                        <th>Average Point Contribution</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $db_query = mysql_query("SELECT * FROM " . $teams_name);
+                    while ($team = mysql_fetch_array($db_query)) {
+                        echo("<tr>");
+                        echo("<td scope=\"row\">");
+                        echo($team['teamNumber']);
+                        echo("</td>");
+                        echo("<td scope=\"row\">");
+                        echo($team['avgScore']);
+                        echo("</td>");
+                        echo("<td scope=\"row\">");
+                        echo("<a href=\"view_team.php?id=" . $_GET['id'] . "&team=" . $team['teamNumber'] . "\" class=\"btn\"><span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span>View</a>");
+                        echo("</td>");
+                        echo("</tr>");
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php
+include('footer.php');
+?>
